be0c89196175b94a78a3138402d42087e67b0d13
Sean Asquith <seanasquith21@gmail.com>
2021-02-05 03:42:26 -0500

    added comments

--- src/com/company/asquith/Main.java
+++ src/com/company/asquith/Main.java
@@ -7,8 +7,8 @@ public class Main {
 
 
     public static void main(String[] args) {
-        double fTemp = 0;
-        while (fTemp >= -460) {
+        double fTemp = 0; //Initializes fahrenheit temperature variable
+        while (fTemp >= -460) { //Exits program after user inputs value less than -460
             fTemp = collectTemp();
             double cTemp = convertTemp(fTemp);
             displayTemp(fTemp, cTemp);
@@ -23,12 +23,12 @@ public class Main {
     }
 
     public static double convertTemp(double fTemp) {
-        return 5 * (fTemp - 32) / 9;
+        return 5 * (fTemp - 32) / 9; //Fahrenheit to Celsius conversion
     }
 
     public static void displayTemp(double fTemp, double cTemp) {
         System.out.print(fTemp + "° Fahrenheit is equal to ");
-        System.out.printf("%.3f", cTemp);
+        System.out.printf("%.3f", cTemp); //Formats celsius temperature to 3 decimal places
         System.out.println("° Celsius\n");
     }
 }
